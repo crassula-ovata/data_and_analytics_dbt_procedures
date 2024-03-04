fd0a31bb5891a61aea7b02c9fcd0d8de2d9a651c
shuclu <slu@dimagi.com>
2024-03-04 15:34:52 -0800

    imported all stored procedures from metadata.procedures

---
+++ macros/sp_aws/create_ sp_run_job.sql
@@ -0,0 +1,19 @@
+{% macro create_sp_run_job() %}
+CREATE OR REPLACE PROCEDURE 
+
+    {% if target.name=='qa' %}
+      METADATA.PROCEDURES_QA
+    {% elif target.name=='prod' %}
+      METADATA.PROCEDURES
+    {% else %}
+      METADATA.PROCEDURES_DEV
+    {% endif %}
+
+.SP_RUN_JOB("JOBNAME" VARCHAR(16777216), "SOURCESQL" VARCHAR(16777216), "TYPE" VARCHAR(16777216), 
+            "SUBTYPE" VARCHAR(16777216), "DOMAIN" VARCHAR(16777216), "DL_DB" VARCHAR(16777216), 
+            "DL_SCHEMA" VARCHAR(16777216), "DM_DB" VARCHAR(16777216), "TASK_ID" NUMBER(38,0))
+    RETURNS VARCHAR(16777216)
+    LANGUAGE SQL
+    EXECUTE AS CALLER
+AS '{{ sp_run_job() }}';
+{% endmacro %}
