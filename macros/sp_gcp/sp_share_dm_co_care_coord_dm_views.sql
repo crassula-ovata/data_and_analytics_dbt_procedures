079a33e76bdd49e27fddf559df52eb8d8abf035a
Joseph Mooney <jmooney@dimagi.com>
2024-07-09 17:51:29 -0400

    Updates to get proper spacing in generation scripts.

--- macros/sp_gcp/sp_share_dm_co_care_coord_dm_views.sql
+++ macros/sp_gcp/sp_share_dm_co_care_coord_dm_views.sql
@@ -1,29 +1,26 @@
-{% macro sp_share_dm_co_care_coord_dm_views() %}
-
-    {% if target.name=='qa-gcp' or target.name=='prod-gcp'%}
-        {% if target.name=='qa-gcp' %}
-            {% set db_name = 'dm_co_care_coord_qa' %}
-            {% set share_name = 'bha_uat_s' %}
-        {% elif target.name=='prod-gcp' %}
-            {% set db_name = 'dm_co_care_coord_prod' %}
-            {% set share_name = 'bha_prod_s' %}
-        {% endif %}
-
-        BEGIN
-            grant select on view {{db_name}}.dm.VW_ARCHIVED_CASES to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_CLIENT_ALIAS to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_CLINIC_PROVIDER to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_ACCESSIBILITY to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_CLINIC_TYPE to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_LANGUAGES_SPOKEN to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_MENTAL_HEALTH_SERVICES to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_PAYERS_ACCEPTED to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_POPULATION_SERVED to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_PROGRAMS to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_SUBSTANCE_USE_SERVICES to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_FORM_METADATA to share {{share_name}};
-            grant select on view {{db_name}}.dm.VW_SERVICE_CLINIC_PROVIDER_CLIENT to share {{share_name}};
-        END
-    {% endif %}
-
-{% endmacro %}
+{%- macro sp_share_dm_co_care_coord_dm_views() -%}
+{%- if target.name=='qa-gcp' or target.name=='prod-gcp' -%}
+    {%- if target.name=='qa-gcp' -%}
+        {%- set db_name = 'dm_co_care_coord_qa' -%}
+        {%- set share_name = 'bha_uat_s' -%}
+    {%- elif target.name=='prod-gcp' -%}
+        {%- set db_name = 'dm_co_care_coord_prod' -%}
+        {%- set share_name = 'bha_prod_s' -%}
+    {%- endif -%}
+BEGIN
+    grant select on view {{db_name}}.dm.VW_ARCHIVED_CASES to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_CLIENT_ALIAS to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_CLINIC_PROVIDER to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_ACCESSIBILITY to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_CLINIC_TYPE to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_LANGUAGES_SPOKEN to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_MENTAL_HEALTH_SERVICES to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_PAYERS_ACCEPTED to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_POPULATION_SERVED to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_PROGRAMS to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_EXPLODE_CLINIC_SUBSTANCE_USE_SERVICES to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_FORM_METADATA to share {{share_name}};
+    grant select on view {{db_name}}.dm.VW_SERVICE_CLINIC_PROVIDER_CLIENT to share {{share_name}};
+END
+{%- endif -%}
+{%- endmacro -%}
