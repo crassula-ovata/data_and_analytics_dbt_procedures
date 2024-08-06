f1683965e0d44603e628b411575b9df8f936552f
Joe Mooney <119339449+Joe-Mooney@users.noreply.github.com>
2024-08-06 16:04:47 -0400

    Update create_sp_share_dm_co_care_coord_dm_views.sql

--- macros/sp_gcp/create_sp_share_dm_co_care_coord_dm_views.sql
+++ macros/sp_gcp/create_sp_share_dm_co_care_coord_dm_views.sql
@@ -1,14 +1,15 @@
 {%- macro create_sp_share_dm_co_care_coord_dm_views() -%}
-CREATE OR REPLACE PROCEDURE 
+CREATE OR REPLACE PROCEDURE METADATA.
     {%- if target.name=='test-gcp' %}
-METADATA.PROCEDURES_TEST.SP_SHARE_DM_CO_CARE_COORD_TEST_DM_VIEWS()
+PROCEDURES_TEST
     {%- elif target.name=='dev-gcp' %}
-METADATA.PROCEDURES_DEV.SP_SHARE_DM_CO_CARE_COORD_DEV_DM_VIEWS()
+PROCEDURES_DEV
     {%- elif target.name=='qa-gcp' %}
-METADATA.PROCEDURES_QA.SP_SHARE_DM_CO_CARE_COORD_QA_DM_VIEWS()
+PROCEDURES_QA
     {%- elif target.name=='prod-gcp' %}
-METADATA.PROCEDURES.SP_SHARE_DM_CO_CARE_COORD_PROD_DM_VIEWS()
-    {%- endif %}
+PROCEDURES
+    {%- endif -%}
+.SP_SHARE_DM_CO_CARE_COORD_DM_VIEWS()
 RETURNS VARCHAR
 LANGUAGE SQL
 AS
